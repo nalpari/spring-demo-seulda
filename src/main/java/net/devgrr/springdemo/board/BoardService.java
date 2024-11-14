4e408cb1361b87bdd178f618a650988de3abe12a
amekimm <amekimm@gmail.com>
2024-11-14 13:33:14 +0900

    refactor: board service에 transaction 추가

--- src/main/java/net/devgrr/springdemo/board/BoardService.java
+++ src/main/java/net/devgrr/springdemo/board/BoardService.java
@@ -9,7 +9,9 @@ import net.devgrr.springdemo.config.exception.ErrorCode;
 import net.devgrr.springdemo.config.mapStruct.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class BoardService {
