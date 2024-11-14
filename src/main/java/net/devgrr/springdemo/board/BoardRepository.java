7aea1f82b280d2ad92d0d16525ae84676eed3043
amekimm <amekimm@gmail.com>
2024-11-14 11:06:01 +0900

    refactor: board repository 내 미사용 메서드 삭제

--- src/main/java/net/devgrr/springdemo/board/BoardRepository.java
+++ src/main/java/net/devgrr/springdemo/board/BoardRepository.java
@@ -1,13 +1,6 @@
 package net.devgrr.springdemo.board;
 
-import java.util.List;
 import net.devgrr.springdemo.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-  Board findByTitle(String title);
-
-  Board findByTitleAndContent(String title, String content);
-
-  List<Board> findByContentLike(String content);
-}
+public interface BoardRepository extends JpaRepository<Board, Integer> {}
