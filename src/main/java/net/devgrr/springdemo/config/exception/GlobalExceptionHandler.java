16d9a44acbddfc3013d5bab7291212392ed4fc04
amekimm <amekimm@gmail.com>
2024-11-15 14:15:53 +0900

    refactor: 에러 발생 시 에러메세지 출력하도록 수정

--- src/main/java/net/devgrr/springdemo/config/exception/GlobalExceptionHandler.java
+++ src/main/java/net/devgrr/springdemo/config/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package net.devgrr.springdemo.config.exception;
 
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,29 +14,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
   protected ResponseEntity<ErrorResponse> handle(HttpRequestMethodNotSupportedException e) {
-    //        System.out.println("error 1 : " + e.getMessage());
+    //    System.out.println("error 1 : " + e.getMessage());
     return ResponseEntity.badRequest().body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED));
   }
 
-  @ExceptionHandler(
-      MethodArgumentNotValidException
-          .class) // ConstraintViolationException // InvocationTargetException
+  @ExceptionHandler(MethodArgumentNotValidException.class)
   protected ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
-    //        System.out.println("error 2 : " + e.getMessage());
+    //    System.out.println("error 2 : " + e.getMessage());
+    final String errorMessages =
+        e.getBindingResult().getFieldErrors().stream()
+            .map(FieldError::getDefaultMessage)
+            .collect(Collectors.joining(", "));
     return ResponseEntity.badRequest()
-        .body(new ErrorResponse(ErrorCode.INVALID_INPUT_VALUE, e.getMessage()));
+        .body(new ErrorResponse(ErrorCode.INVALID_INPUT_VALUE, errorMessages));
   }
 
   @ExceptionHandler(Exception.class)
   protected ResponseEntity<ErrorResponse> handle(Exception e) {
-    //        System.out.println("error 3 : " + e.getMessage());
+    //    System.out.println("error 3 : " + e.getMessage());
     return ResponseEntity.internalServerError()
-        .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+        .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage()));
   }
 
   @ExceptionHandler(BaseException.class)
   protected ResponseEntity<ErrorResponse> handle(BaseException e) {
-    //        System.out.println("error 4 : " + e.getMessage());
+    //    System.out.println("error 4 : " + e.getMessage());
     final ErrorCode errorCode = e.getErrorCode();
     final String errorMessage = e.getMessage();
     return ResponseEntity.status(errorCode.getStatus())
