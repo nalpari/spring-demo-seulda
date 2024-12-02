e26b308c056bb41d529f5be892712856498c27b1
amekimm <amekimm@gmail.com>
2024-12-02 14:32:49 +0900

    refactor: init sql member 데이터 merge로 변경
    (insert into ... on conflict do nothing)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
-INSERT INTO member (user_id, password, email, name, ROLE, created_at, updated_at)
-VALUES ('user1', '$2a$10$z1NLrjhkoPJYzHucx7XVDeENGlYgs1zPt/hQIgtAqentc.Qrkv7m6', 'user1@naver.com',
-        'user1', 'USER', now(), NULL);
-INSERT INTO member (user_id, password, email, name, ROLE, created_at, updated_at)
-VALUES ('user2', '$2a$10$z1NLrjhkoPJYzHucx7XVDeENGlYgs1zPt/hQIgtAqentc.Qrkv7m6', 'user2@google.com',
-        'user2', 'USER', now(), NULL);
+MERGE INTO member (user_id, password, email, name, ROLE, created_at, updated_at)
+    KEY (user_id)
+    VALUES ('user1', '$2a$10$z1NLrjhkoPJYzHucx7XVDeENGlYgs1zPt/hQIgtAqentc.Qrkv7m6',
+            'user1@naver.com',
+            'user1', 'USER', now(), NULL);
+MERGE INTO member (user_id, password, email, name, ROLE, created_at, updated_at)
+    KEY (user_id)
+    VALUES ('user2', '$2a$10$z1NLrjhkoPJYzHucx7XVDeENGlYgs1zPt/hQIgtAqentc.Qrkv7m6',
+            'user2@google.com',
+            'user2', 'USER', now(), NULL);
