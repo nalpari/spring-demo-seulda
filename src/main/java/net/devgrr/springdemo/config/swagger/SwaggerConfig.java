d1b8d3d74988b2d61f142ffdf76b38dc7478535e
amekimm <amekimm@gmail.com>
2024-12-16 17:49:50 +0900

    refactor: Swagger에서 token을 추가할 수 있도록 config 수정

--- src/main/java/net/devgrr/springdemo/config/swagger/SwaggerConfig.java
+++ src/main/java/net/devgrr/springdemo/config/swagger/SwaggerConfig.java
@@ -1,17 +1,47 @@
 package net.devgrr.springdemo.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+  @Value("${jwt.access.header}")
+  private String accessHeader;
+
+  @Value("${jwt.refresh.header}")
+  private String refreshHeader;
+
   @Bean
   public OpenAPI openAPI() {
     OpenAPI openAPI = new OpenAPI();
     openAPI.info(new Info().title("DEMO API").version("1.0").description("DEMO API Documentation"));
+    openAPI.addSecurityItem(new SecurityRequirement().addList(accessHeader));
+    openAPI.addSecurityItem(new SecurityRequirement().addList(refreshHeader));
+    openAPI.components(
+        new Components()
+            .addSecuritySchemes(
+                accessHeader,
+                new SecurityScheme()
+                    .name(accessHeader)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .in(SecurityScheme.In.HEADER)
+                    .bearerFormat("JWT"))
+            .addSecuritySchemes(
+                refreshHeader,
+                new SecurityScheme()
+                    .name(refreshHeader)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .in(SecurityScheme.In.HEADER)
+                    .bearerFormat("JWT")));
     return openAPI;
   }
 }
