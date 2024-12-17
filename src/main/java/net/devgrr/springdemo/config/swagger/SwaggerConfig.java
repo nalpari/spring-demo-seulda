d33d67730285c84d425b31e75807be4dd13f55fb
amekimm <amekimm@gmail.com>
2024-12-17 16:19:16 +0900

    feat: swagger에 login api 추가

--- src/main/java/net/devgrr/springdemo/config/swagger/SwaggerConfig.java
+++ src/main/java/net/devgrr/springdemo/config/swagger/SwaggerConfig.java
@@ -2,9 +2,21 @@ package net.devgrr.springdemo.config.swagger;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +54,32 @@ public class SwaggerConfig {
                     .scheme("bearer")
                     .in(SecurityScheme.In.HEADER)
                     .bearerFormat("JWT")));
+    // login endpoint
+    openAPI.path("/login", createLoginPathItem());
+
     return openAPI;
+  }
+
+  private PathItem createLoginPathItem() {
+    Map<String, Schema> properties = new HashMap<>();
+    properties.put("userId", new StringSchema());
+    properties.put("password", new StringSchema());
+    Schema<?> schema = new ObjectSchema().properties(properties);
+    RequestBody requestBody =
+        new RequestBody()
+            .content(
+                new Content()
+                    .addMediaType(
+                        org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
+                        new MediaType().schema(schema)));
+    Operation operation =
+        new Operation()
+            .requestBody(requestBody)
+            .responses(
+                new ApiResponses()
+                    .addApiResponse("200", new ApiResponse().description("OK"))
+                    .addApiResponse("400", new ApiResponse().description("Bad Request")))
+            .addTagsItem("login");
+    return new PathItem().post(operation);
   }
 }
